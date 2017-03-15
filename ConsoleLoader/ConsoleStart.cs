116d2c2118ada9f635e79ca32a24ef81fba8ee27
Arbiterh <arbiterh@gmail.com>
2017-03-16 02:09:04 +0700

    Added Console Loader.

---
+++ ConsoleLoader/ConsoleStart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLoader
+{
+    class ConsoleStart
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
